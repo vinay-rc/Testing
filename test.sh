b62a426f42521b433781970d107c212828d4a1da
EC2 Default User <ec2-user@ip-10-1-0-213.ec2.internal>
2022-11-08 08:41:17 +0000

    added name to code

--- test.sh
+++ test.sh
@@ -2,3 +2,10 @@
 # This is a comment!
 echo Hello World
 
+# Author : Zara Ali
+# Copyright (c) Tutorialspoint.com
+# Script follows here:
+
+echo "What is your name?"
+read PERSON
+echo "Hello, $PERSON"
